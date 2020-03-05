d9430b1f434c2ec489331cff4686789fa8fc0c24
firedial <42674772+firedial@users.noreply.github.com>
2020-03-05 22:30:25 +0900

    移動用のレコードを一つだけでいれれないようにした

--- interactor/balance.go
+++ interactor/balance.go
@@ -2,6 +2,7 @@ package interactor
 
 import (
     "github.com/firedial/midas-misuzu/entity"
+    "github.com/firedial/midas-misuzu/config"
 )
 
 func GetBalance(queries map[string][]string) (entity.Balances, error) {
@@ -9,6 +10,19 @@ func GetBalance(queries map[string][]string) (entity.Balances, error) {
 }
 
 func InsertBalances(balance entity.Balance) string {
+
+    if balance.KindId == config.KIND_MOVE_ID {
+        return "\"Can not use kind move id!\""
+    }
+
+    if balance.PurposeId == config.PURPOSE_MOVE_ID {
+        return "\"Can not use purpose move id!\""
+    }
+
+    if balance.PlaceId == config.PLACE_MOVE_ID {
+        return "\"Can not use place move id!\""
+    }
+
     balances := []entity.Balance{balance}
 
     if !entity.IsSuitableBalances(balances) {
