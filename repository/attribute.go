788c41bd5eae357dc185b6c0b7535a24b2d26226
firedial <42674772+firedial@users.noreply.github.com>
2021-02-19 23:48:47 +0900

    relation の作成

--- repository/attribute.go
+++ repository/attribute.go
@@ -5,4 +5,13 @@ import "github.com/firedial/midas-misuzu/entity"
 type AttributeRepository interface {
     FindAllElement(string) (entity.AttributeElements, error)
     SaveElement(string, entity.AttributeElement) error
+
+    FindAllCategory(string) (entity.AttributeCategories, error)
+    SaveCategory(string, entity.AttributeCategory) error
+
+    FindAllGroup(string) (entity.AttributeGroups, error)
+    SaveGroup(string, entity.AttributeGroup) error
+
+    FindAllGroupRelation(string) (entity.AttributeGroupRelations, error)
+    SaveGroupRelation(string, entity.AttributeGroupRelation) error
 }
