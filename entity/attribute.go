574636eb1e7a9ef285bca45d67683a27e865a771
firedial <42674772+firedial@users.noreply.github.com>
2021-02-19 23:29:27 +0900

    attribute のいろいろな entity の作成

--- entity/attribute.go
+++ entity/attribute.go
@@ -8,3 +8,26 @@ type AttributeElement struct {
 }
 
 type AttributeElements []AttributeElement
+
+type AttributeCategory struct {
+    Id int `json:"id"`
+    Name string `json:"name"`
+    Description string `json:"description"`
+}
+
+type AttributeCategories []AttributeCategory
+
+type AttributeGroup struct {
+    Id int `json:"id"`
+    Name string `json:"name"`
+    Description string `json:"description"`
+}
+
+type AttributeGroups []AttributeGroup
+
+type AttributeGroupRelation struct {
+    GroupId int `json:"group_id"`
+    ElementId int `json:"element_id"`
+}
+
+type AttributeGroupRelations []AttributeGroupRelation
